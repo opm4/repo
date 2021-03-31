2239b6f9ef5ce78f5d61b9a0648e12f28720d0a5
opm4 <mateioprea@gmail.com>
2021-03-31 18:27:52 +0300

    updated game logic

---
+++ pocker/get_winner.py
@@ -0,0 +1,12 @@
+# logic behing deciding the winner
+
+#  player_cards shoudl be a list of list
+# containing [[('j','diamonds'),('k', 'clubs')], ['2','hearts']
+cards = ['2', '3', '4', '5', '6', '7',
+         '8', '9', '10', 'J', 'Q', 'K', 'A']
+colors = ['spades', 'hearts', 'clubs', 'diamonds']
+
+
+def get_winner(players_and_deck_cards):
+    # get each player cars
+    lenght
